42c2e80c2c3a262d18777f74c9282ac5d2c16d07
chiedozieezenwa <chiedozieezenwa@gmail.com>
2024-05-09 00:46:54 +0100

    navbar and hero css modules

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import Hero from "../components/Hero/Hero"
 import Navbar from "../components/Navbar/Navbar"
 
 
@@ -5,6 +6,7 @@ export default function Home() {
   return (
     <div>
         <Navbar />
+        <Hero />
     </div>
   )
 }
