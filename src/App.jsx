3d3fed3e5f43fc9791496041bc17ebd61cc064b1
Daniel Udechukwu <danieludechukwu117@gmail.com>
2024-05-10 03:24:24 +0100

    :construction: Added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import { Route, Routes } from "react-router-dom";
+
 const App = () => {
   return (
-    <>
-      <div>
-        <h1>Hello Kidera</h1>
-      </div>
-    </>
+    <Routes>
+      {/* Add your routes like this */}
+      {/* <Route path="/" element={<Home />} /> */}
+    </Routes>
   );
 };
 
