cfdfb85afcbccd4431cc71b6d9e68d6e5f265be0
chiedozieezenwa <chiedozieezenwa@gmail.com>
2024-05-10 12:47:27 +0100

    Resolved Conflicts

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import styles from './Navbar.module.css'
 import kidera from '../../assets/Kidera.png'
+import { Link } from 'react-router-dom'
 
 export default function Navbar() {
   return (
@@ -16,7 +17,9 @@ export default function Navbar() {
                     <li className={styles['nav-list-item']}>Courses</li>
                 </ul>
 
+                <Link to="/SignUp">
                 <button className={styles['nav-button']}>Login</button>
+                </Link>
             </div>
         </nav>
     </header>
