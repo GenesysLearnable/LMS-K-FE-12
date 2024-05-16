fa6d8132f1af2ad0962efd1735104d7fde40d675
chiedozieezenwa <chiedozieezenwa@gmail.com>
2024-05-16 23:42:08 +0100

    about page setup

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,10 +1,28 @@
 import Navbar from "../../components/Navbar/Navbar";
+import Footer from "../../components/Footer/Footer"
+import styles from './About.module.css'
+import AboutNav from "./AboutNav";
+import { Routes, Route } from "react-router-dom";
+import Mission from "./Mission";
+import Approach from "../About/Approach"
+import Contact from "./Contact"
 
 const About = () => {
   return (
     <>
-      <Navbar />
-      <h1 style={{ marginTop: "8rem" }}>About</h1>
+        <div>
+        <Navbar />
+        <h2 className={styles.title}>About us!</h2>
+        <AboutNav />
+          <div className={styles.content}>
+            <Routes>
+              <Route path="/" element={<Mission />} />
+              <Route path="/Approach" element={<Approach />} />
+              <Route path="/Contact" element={<Contact />} />
+            </Routes>
+          </div>
+        <Footer />
+        </div>
     </>
   );
 };
--- src/pages/About/AboutNav.jsx
+++ src/pages/About/AboutNav.jsx
@@ -1,12 +1,13 @@
 import styles from './About.module.css'
+import { Link } from 'react-router-dom'
 
 export default function AboutNav() {
   return (
         <nav className={styles.container}>
-            <ul>
-                <li>Mission</li>
-                <li>Approach</li>
-                <li>Contact Us</li>
+            <ul className={styles.navlist}>
+                <li className={styles.navlistitem}><Link to="/about">Mission</Link></li>
+                <li className={styles.navlistitem}><Link to="/Approach">Approach</Link></li>
+                <li className={styles.navlistitem}><Link to="/Contact">Contact Us</Link></li>
             </ul>
         </nav>
   )
